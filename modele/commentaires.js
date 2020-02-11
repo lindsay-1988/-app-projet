7d9a5d28465458366dc4613138462ccf9dbf692a
Lindsay fraynet <lindsay.fraynet@gmail.com>
2020-02-11 13:00:24 +0100

    first commit

--- modele/commentaires.js
+++ modele/commentaires.js
@@ -4,7 +4,7 @@ const joi = require("@hapi/joi").extend(require("@hapi/joi-date"));
 // schema des données => colonnes
 const schemaCommentaires = mongoose.Schema({
     contenu : String,
-    datereation : Date,
+    dateCreation : Date,
     nomAuteur : String,
 });
 // lier le schéma à la collection = Modèle
@@ -12,7 +12,7 @@ const Commentaires = mongoose.model("commentaire", schemaCommentaires);
 
 const schema = joi.object({
     contenu : joi.string().min(3).max(1000).required(),
-    datereation : joi.date().format('YYYY-MM-DD').required(),
+    dateCreation : joi.date().format('YYYY-MM-DD').required(),
     nomAuteur : joi.number().min(3).max(255).required(),
 });
 
